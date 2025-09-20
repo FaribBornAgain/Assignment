45bc77a84715de8371ad8b58e5fa88540ee718e4
FaribBornAgain <sadidsiddiquev3@gmail.com>
2025-09-21 04:03:41 +0600

    Added Grade.java

---
+++ Grade.java
@@ -0,0 +1,12 @@
+public class Grade {
+    public String getGrade(int marks) {
+        if (marks >= 90)
+            return "A";
+        else if (marks >= 75)
+            return "B";
+        else if (marks >= 50)
+            return "C";
+        else
+            return "F";
+    }
+}
